3d51607cccfad51d464e032175c08f621c8b3a73
sa77irak <sa77irak@gmail.com>
2019-08-11 12:11:13 +0900

    added function constructor, data structure, and an addItem method in budget controller

--- app-copy.js
+++ app-copy.js
@@ -2,10 +2,63 @@
 // BUDGET CONTROLLER
 var budgetController = (function() {
 
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+
+
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0,
+        },
+        budget: 0,
+        percentage: -1,
+    };
+
     return {
-        addItem: 
+        addItem: function(type, des, val) {
+            var newItem,ID;
+
+            // Create new ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            // Create new item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type ==='inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            // Push it into our data structure
+            data.allItems[type].push(newItem);
+
+            // Return the new element
+            return newItem;
+
+        },
 
     }
+
 })();
 
 
